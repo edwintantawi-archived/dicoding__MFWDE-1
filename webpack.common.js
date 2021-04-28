8b8ae14a454c7c3b1e654414ab7a49fa2714ab62
edwintantawi <edwintantawi@gmail.com>
2021-04-28 18:20:53 +0700

    added logo and favicon

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const path = require('path');
 
 module.exports = {
@@ -35,6 +36,9 @@ module.exports = {
           to: path.resolve(__dirname, 'dist/'),
         },
       ],
+    }),
+    new FaviconsWebpackPlugin({
+      logo: path.resolve(__dirname, 'src/public/images/logo/favicon.png'),
     }),
   ],
 };
