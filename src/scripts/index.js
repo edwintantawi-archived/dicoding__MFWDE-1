fcb13173c4b81649f5758dbb2f4734ad226b227b
edwintantawi <edwintantawi@gmail.com>
2021-04-29 00:27:05 +0700

    added list of restaurants from DATA.json

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,8 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.css';
 
+import { restaurants } from '../DATA.json';
+
 const navMenu = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 
@@ -12,4 +14,36 @@ navToggle.addEventListener('click', (e) => {
   } else {
     navMenu.classList.remove('nav--active');
   }
+});
+
+const listContent = document.querySelector('#root-content');
+
+restaurants.forEach((restaurant, idx) => {
+  listContent.innerHTML += `
+    <div class="list__item">
+            <img
+              src="${restaurant.pictureId}"
+              alt="${restaurant.name}"
+              class="list__image"
+            />
+            <div class="list__content">
+              <div class="list__data">
+                <h2 class="list__title">${restaurant.name}</h2>
+                <p class="list__description">
+                  ${restaurant.description}
+                </p>
+              </div>
+              <div class="list__info">
+                <div>
+                  <h3>Rating:</h3>
+                  <span>${restaurant.rating}</span>
+                </div>
+                <div>
+                  <h3>City:</h3>
+                  <span>${restaurant.city}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+  `;
 });
